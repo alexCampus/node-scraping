9353b7b9f93c41009ceb1f2f32a812e4dd3df6b6
alexCampus <alex.depem@gmail.com>
2020-04-09 16:34:37 +0200

    update scrapper cuisine Az Cyril Lignac

--- app.js
+++ app.js
@@ -22,19 +22,19 @@ app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
-const port = 9000
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const port = 9000;
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- scraper.js
+++ scraper.js
@@ -1,43 +1,97 @@
-const cheerio = require("cheerio");
-const axios = require("axios");
+const cheerio = require('cheerio');
+var db = require('./utils/connection');
 
-const siteUrl = "https://remoteok.io/";
+const axios = require('axios');
+const prefixUrl = 'https://www.cuisineaz.com/recettes/';
+const recipe = 'poulet-au-citron-facon-tajine-de-cyril-lignac-110242.aspx';
+// const siteUrl = 'galette-de-pommes-de-terre-salade-de-roquette-tomates-de-cyril-lignac-110240.aspx';
+//const siteUrl = 'petits-pots-de-creme-a-la-vanille-de-cyril-lignac-110241.aspx';
+// const siteUrl = 'penne-au-pesto-basilic-de-cyril-lignac-110238.aspx';
+const siteUrl = prefixUrl + recipe;
 
-let siteName = "";
-const categories = new Set();
-const tags = new Set();
-const locations = new Set();
-const positions = new Set();
-
-const fetchData = async () => {
-  const result = await axios.get(siteUrl);
-  return cheerio.load(result.data);
+let recetteName = '';
+let img = '';
+let categorie = '';
+let insertedId = null;
+let indexUstensileBegin = null;
+const ingredients = new Set();
+const etapes = new Set();
+const textEtapes = new Set();
+const fetchData = async() => {
+    const result = await axios.get(siteUrl);
+    return cheerio.load(result.data);
 };
 
-const getResults = async () => {
-  const $ = await fetchData();
+const getResults = async() => {
+    const $ = await fetchData();
 
-  siteName = $('.top > .action-post-job').text();
+    recetteName = $('header h1').text();
+    img = $('#ContentPlaceHolder_recipeImg').data('src');
+    categorie = $('#compass ol li:nth-child(3) span').text();
+    let payloadRecette = {
+        name: recetteName,
+        img: img,
+        categorie: categorie
+    };
+    $('.recipe_ingredients li span').each((index, element) => {
+        if ($(element).text() === 'Les ustensiles :' ||$(element).text() === ' Les ustensiles :' || $(element).text() === 'Les ustensiles : ' || $(element).text() === 'Ustensiles :'|| $(element).text() === 'Ustensiles : ') {
+            indexUstensileBegin = index;
+        }
+        ingredients.add($(element).text().replace('•', '').trim());
+    });
+    $('.recipe_instructions p span').each((index, element) => {
+        etapes.add($(element).text());
+    });
+    $('.recipe_instructions p').each((index, element) => {
+        etapes.add($(element).children().text());
+        textEtapes.add($(element).text().replace($(element).children().text(), ''));
+    });
+    if (indexUstensileBegin === null) {
+        indexUstensileBegin = ingredients.size;
+    }
 
-  $(".tags .tag").each((index, element) => {
-    tags.add($(element).text());
-  });
-  $(".location").each((index, element) => {
-    locations.add($(element).text());
-  });
-  $("div.nav p").each((index, element) => {
-    categories.add($(element).text());
-  });
-  $('.company_and_position [itemprop="title"]').each((index, element) => {
-    positions.add($(element).text());
-  });
-  return {
-    positions: [...positions].sort(),
-    tags: [...tags].sort(),
-    locations: [...locations].sort(),
-    categories: [...categories].sort(),
-    siteName,
-  };
+    //insert in database
+    insertedId = db.query('INSERT INTO recettes SET ?', payloadRecette, function(err, rows) {
+        [...ingredients].slice(0, indexUstensileBegin).forEach(el => {
+            let payloadIngredients = {
+                content: el,
+                recette_id: rows.insertId
+            };
+            db.query('INSERT INTO ingredients SET ?', payloadIngredients, function(err, rows) {
+
+            });
+        });
+        [...ingredients].slice(indexUstensileBegin + 1).forEach(el => {
+            let payloadUstensiles = {
+                content: el,
+                recette_id: rows.insertId
+            };
+            db.query('INSERT INTO ustensiles SET ?', payloadUstensiles, function(err, rows) {
+
+            });
+        });
+
+        [...etapes].forEach((el, index) => {
+            let payloadEtapes = {
+                number: index + 1,
+                content: [...textEtapes][index],
+                recette_id: rows.insertId
+            };
+            db.query('INSERT INTO etapes SET ?', payloadEtapes, function(err, rows) {
+
+            });
+        });
+
+    });
+
+    return {
+        etapes: [...etapes],
+        textEtapes: [...textEtapes],
+        ingredients: [...ingredients].slice(0, indexUstensileBegin),
+        ustensiles: [...ingredients].slice(indexUstensileBegin + 1),
+        recetteName,
+        img
+    };
 };
 
 module.exports = getResults;
